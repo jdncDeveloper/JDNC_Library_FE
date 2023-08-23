aa621302441debf1852791a519ec0697fd7acc19
Warizi <jjin9050@naver.com>
2023-08-23 09:36:58 +0900

    feat: refresh 토큰 axios config 설정

--- src/api/axiosLoginConfig.js
+++ src/api/axiosLoginConfig.js
@@ -6,8 +6,12 @@ const axiosLoginConfig = axios.create({
 axiosLoginConfig.interceptors.response.use(
   response => {
     const newToken = response.headers.authorization;
+    const newRefreshToken = response.headers['authorization-refresh'];
     if (newToken) {
       localStorage.setItem('jdncLibAccessToken', newToken);
+    }
+    if (newRefreshToken) {
+      localStorage.setItem('jdncLibRefreshToken', newRefreshToken);
     }
     return response;
   },
