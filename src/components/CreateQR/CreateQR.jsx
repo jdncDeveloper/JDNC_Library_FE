ff9ed551881bf5efd3eaabf1d2805803e7c3fd1c
Warizi <jjin9050@naver.com>
2023-08-29 15:02:21 +0900

    feat: Qr code 북넘버 추가

--- src/components/CreateQR/CreateQR.jsx
+++ src/components/CreateQR/CreateQR.jsx
@@ -1,16 +1,30 @@
 import Style from './CreateQR.style';
 import { QRCodeCanvas } from 'qrcode.react';
+import { useRef } from 'react';
 import { useState } from 'react';
 
 const CreateQR = () => {
   const [ idValue, setIdValue ] = useState('');
   const [ fileName, setFileName ] = useState('QR_Book_');
+  const [bookNumberCanvasImage, setBookNumberCanvasImage] = useState('null');
   const baseUrl = window.location.origin;
-    
+  const bookNumberCanvas = useRef(null);
+
   function handleBookNumber(event) {
     const value = event.target.value;
     if(Number(value) || value === '') {
       setIdValue(value);
+      const context = bookNumberCanvas.current.getContext('2d');
+      context.clearRect(0, 0, bookNumberCanvas.current.width, bookNumberCanvas.current.height);
+      context.fillStyle = 'white';
+      context.fillRect(0, 0, bookNumberCanvas.current.width, bookNumberCanvas.current.height);
+      context.fillStyle = 'black';
+      context.strokeRect(2, 2, bookNumberCanvas.current.width - 4, bookNumberCanvas.current.height - 4);
+      context.font = '15px Arial';
+      if(value.length === 1) context.fillText(`  ${value}`, 1, 15);
+      if(value.length === 2) context.fillText(` ${value}`, 1, 15);
+      if(value.length >= 3) context.fillText(`${value}`, 1, 15);
+      setBookNumberCanvasImage(bookNumberCanvas.current.toDataURL('image/jpg'));
     }
   }
   const handleFileName = (event) => {
@@ -18,8 +32,9 @@ const CreateQR = () => {
     setFileName(value);
   }
 
+  const canvasDom = document.querySelector('canvas');
+
   function imageDownload() {
-    const canvasDom = document.querySelector('canvas');
     const imageCanvas = canvasDom.toDataURL('image/png');
 
     const downloadLink = document.createElement("a");
@@ -31,8 +46,11 @@ const CreateQR = () => {
     <>
       <Style.QrContainer>
         <div>
-          <QRCodeCanvas value={`${baseUrl}/borrowbook/qr/${idValue}`} />
-          <span>{`책번호: ${idValue}번`}</span>
+          <QRCodeCanvas value={`${baseUrl}/borrowbook/qr/${idValue}`}
+          size={150} 
+          imageSettings={{src: bookNumberCanvasImage, height: 20, width: 30}}/>
+          <span>{`책번호`}</span>
+          <canvas ref={bookNumberCanvas} width={'30px'} height={'20px'}></canvas>
         </div>
         <div>
           <Style.QRInput placeholder='bookId를 입력하세요.' value={idValue} onChange={handleBookNumber}/>
